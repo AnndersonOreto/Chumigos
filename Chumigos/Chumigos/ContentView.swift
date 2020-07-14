115db374e1f2ecff29d321727e52f2239468e8e9
Annderson Oreto <anndersonp@gmail.com>
2020-07-14 17:22:15 -0300

    First commit

---
+++ Chumigos/Chumigos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 14/07/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
