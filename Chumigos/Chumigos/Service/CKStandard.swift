d08656291491f0967f33f7b8574b6bf8774ad2f6
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-07-15 14:49:26 -0300

    Cloudkit Setup

---
+++ Chumigos/Chumigos/Service/CKStandard.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  Chumigos
+//
+//  Created by Marcus Vinicius Vieira Badiale on 15/07/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class CKStandard {
+    
+    
+    
+}
