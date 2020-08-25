46839b46cc95118bcf704a3677718499387a404b
Annderson Oreto <anndersonp@gmail.com>
2020-08-25 16:39:25 -0300

    End Game Screen

---
+++ Chumigos/Chumigos/Games/Components/EndGameView/View/EndGameView.swift
@@ -0,0 +1,21 @@
+//
+//  EndGameView.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 25/08/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import SwiftUI
+
+struct EndGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EndGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        EndGameView()
+    }
+}
---
+++ Chumigos/Chumigos/Games/Components/EndGameView/ViewModel/EndGameViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  EndGameViewModel.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 25/08/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
