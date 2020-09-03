985786a17287fbfc6bde8b8cbac20c7bab102ce4
Annderson Oreto <anndersonp@gmail.com>
2020-09-03 14:34:01 -0300

    New branch - testing github with discord integration

---
+++ Chumigos/Chumigos/Configuration Screen/View Model/ConfigurationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ConfigurationViewModel.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 03/09/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class ConfigurationViewModel: ObservableObject {
+    
+    
+}
---
+++ Chumigos/Chumigos/Configuration Screen/View/ConfigurationView.swift
@@ -0,0 +1,21 @@
+//
+//  ConfigurationView.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 03/09/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ConfigurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfigurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigurationView()
+    }
+}
