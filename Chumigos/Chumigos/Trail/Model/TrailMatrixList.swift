938f7f1f447d22ffd09d0df03c802f9daaacbb8f
Annderson Oreto <anndersonp@gmail.com>
2020-09-08 14:43:34 -0300

    Trail data persistence back bone

---
+++ Chumigos/Chumigos/Trail/Model/TrailMatrixList.swift
@@ -0,0 +1,18 @@
+//
+//  TrailMatrixList.swift
+//  Chumigos
+//
+//  Created by Annderson Packeiser Oreto on 08/09/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
+
+struct TrailMatrixList: Codable {
+    
+    var matrixList: [[[String]]] = []
+    
+    func convert() -> [TrailSection] {
+        return []
+    }
+}
