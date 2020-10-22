ce16d9811e5c8d34e0b0fd1823342db35b38b2b0
Guilherme Piccoli <guuipiccoli@gmail.com>
2020-10-22 15:02:48 -0300

    unused code

--- Chumigos/Chumigos/Service/DatabaseManager.swift
+++ Chumigos/Chumigos/Service/DatabaseManager.swift
@@ -30,43 +30,7 @@ class DatabaseManager {
             }
         }
     }
-    
-//    func saveFeelings(userUid: String, completion: @escaping(FeelingsInfoArray)->()) {
-//        
-//        let userEmotions: String = FeelingsInfo.sharedInstance.user_emotions.reduce("") { text, name in "\(text),\(name)" }
-//        
-//        let date = Date()
-//        
-//        let dateFormatter = DateFormatter()
-//        
-//        dateFormatter.dateFormat = "hh:mm dd/MM/yyyy"
-//        
-//        FeelingsInfo.sharedInstance.date = dateFormatter.string(from: date)
-//        
-//        let post = ["user_feeling": FeelingsInfo.sharedInstance.user_feeling,
-//                    "user_emotions": userEmotions,
-//                    "user_situation": FeelingsInfo.sharedInstance.user_situation,
-//                    "user_thoughts": FeelingsInfo.sharedInstance.user_thoughts,
-//                    "user_action": FeelingsInfo.sharedInstance.user_action,
-//                    "date": dateFormatter.string(from: date),
-//                    "image": FeelingsInfo.sharedInstance.image]
-//        
-//        ref.child("users").child(userUid).child("feelings").childByAutoId().setValue(post) { (error, ret) in
-//            
-//            if let error = error {
-//                
-//                print(error.localizedDescription)
-//            } else {
-//                
-//                self.getFeelings(userUid: userUid, completion: { data in
-//                    
-//                    fakeReports = data.user_array
-//                    completion(data)
-//                })
-//            }
-//        }
-//    }
-    
+
     func sendPending(source: String, target: String) {
         
         let post = ["pending": source]
@@ -105,7 +69,7 @@ class DatabaseManager {
         }
     }
     
-    func getUserProfile(userUid: String, completion: @escaping(AuthenticationProfile)-> Void) {
+    func getUserProfile(userUid: String, completion: @escaping(AuthenticationProfile) -> Void) {
 
         ref.child("users").child(userUid).observeSingleEvent(of: .value, with: { (snapshopt) in
 
@@ -120,64 +84,7 @@ class DatabaseManager {
         })
     }
     
-//    func getFeelings(userUid: String, completion: @escaping(FeelingsInfoArray)->()) {
-//
-//        ref.child("users").child(userUid).child("feelings").observeSingleEvent(of: .value, with: { (snapshot) in
-//
-//            if let value = snapshot.value as? NSDictionary {
-//
-//                var feelings: [Feelings] = []
-//
-//                for val in value {
-//
-//                    let feelingDict = val.value as? NSDictionary
-//                    let user_feeling = feelingDict?["user_feeling"] as? String ?? ""
-//                    let user_emotions: String = feelingDict?["user_emotions"] as? String ?? ""
-//                    let user_situation: String = feelingDict?["user_situation"] as? String ?? ""
-//                    let user_thoughts: String = feelingDict?["user_thoughts"] as? String ?? ""
-//                    let user_action: String = feelingDict?["user_action"] as? String ?? ""
-//                    let date: String = feelingDict?["date"] as? String ?? ""
-//                    let image: String = feelingDict?["image"] as? String ?? ""
-//                    let feeling = Feelings(user_feeling: user_feeling, user_emotions: user_emotions, user_situation: user_situation, user_thoughts: user_thoughts, user_action: user_action, date: date, image: image)
-//                    feelings.append(feeling)
-//                }
-//
-//                let feelingsArray: FeelingsInfoArray = FeelingsInfoArray(user_array: feelings)
-//
-//                completion(feelingsArray)
-//            } else {
-//                print("error - getFeelings")
-//            }
-//        })
-//    }
-    
-//    func getPacientsList(userUid: String, completion: @escaping([Patient])->()) {
-//
-//        ref.child("users").child(userUid).child("professional_list").observeSingleEvent(of: .value, with: { (snapshot) in
-//
-//            if let value = snapshot.value as? NSDictionary {
-//
-//                var pacients: [Patient] = []
-//
-//                for val in value {
-//
-//                    let patientDict = val.value as? NSDictionary
-//                    let name = patientDict?["name"] as? String ?? ""
-//                    let phone = patientDict?["phone"] as? String ?? ""
-//                    let email = patientDict?["email"] as? String ?? ""
-//
-//                    let patient = Patient(name: name, email: email, phoneNumber: phone)
-//                    pacients.append(patient)
-//                }
-//
-//                completion(pacients)
-//            } else {
-//                print("error - getPacientsList")
-//            }
-//        })
-//    }
-    
-    func getPendingStatus(userUid: String, completion: @escaping(String)-> Void) {
+    func getPendingStatus(userUid: String, completion: @escaping(String) -> Void) {
         ref.child("users").child(userUid).observeSingleEvent(of: .value) { (snapshot) in
             if let value = snapshot.value as? NSDictionary {
                 if let pendingEmail = value["pending"] as? String {
