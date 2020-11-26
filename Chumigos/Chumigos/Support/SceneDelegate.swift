3f28bc7e3c06ad571326c00ce1496d31185c3b9f
Annderson Oreto <anndersonp@gmail.com>
2020-11-26 15:30:35 -0300

    Sound fixed when entering background not stopping and replaying where it stopped on entering back in foreground

--- Chumigos/Chumigos/Support/SceneDelegate.swift
+++ Chumigos/Chumigos/Support/SceneDelegate.swift
@@ -68,12 +68,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
+        // Music plays again when back from background
+        SoundManager.shared.audioPlayerMusic?.play()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        g
+        SoundManager.shared.audioPlayerMusic?.pause()
         
         let current = UNUserNotificationCenter.current()
 
